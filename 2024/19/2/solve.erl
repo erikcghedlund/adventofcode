3c86dd247cc6aa3cf2fda33b8abdba6cc0903877
Erik Hedlund <erikcghedlund@outlook.com>
2024-12-19 21:14:49 +0000

    solve day 19 part 2

--- 2024/19/2/solve.erl
+++ 2024/19/2/solve.erl
@@ -1,37 +1,52 @@
 -module(solve).
 -export([main/0]).
+-define(MEMOIZED,).
 
 parse([Line | Tail]) ->
-    Towels = lists:map(fun (Str) -> lists:delete($,, Str) end, string:lexemes(Line, [$\s])),
+    Towels = lists:map(fun(Str) -> lists:delete($,, Str) end, string:lexemes(Line, [$\s])),
     {Towels, Tail}.
 
-memoized_can_fold(_, []) -> [true];
-memoized_can_fold(Towels, Pattern) -> case erlang:get({'memoized_can_fold', Towels, Pattern}) of
-                                          Ans when is_list(Ans) -> Ans;
-                                          'undefined' ->
-    Help = fun (Towel) -> case lists:prefix(Towel, Pattern) of
-     false -> [false];
-     true -> memoized_can_fold(Towels, lists:nthtail(length(Towel), Pattern))
-                                end
-                end,
+-ifdef(MEMOIZED).
+can_fold(_, []) ->
+    1;
+can_fold(Towels, Pattern) ->
+    case erlang:get({'memoized_can_fold', Towels, Pattern}) of
+        Ans when is_integer(Ans) ->
+            Ans;
+        'undefined' ->
+            Help = fun(Towel) ->
+                case lists:prefix(Towel, Pattern) of
+                    false -> 0;
+                    true -> can_fold(Towels, lists:nthtail(length(Towel), Pattern))
+                end
+            end,
+            Res = lists:sum(lists:map(Help, Towels)),
+            erlang:put({'memoized_can_fold', Towels, Pattern}, Res),
+            Res
+    end.
+-else.
+can_fold(_, []) ->
+    1;
+can_fold(Towels, Pattern) ->
+    Help = fun(Towel) ->
+        case lists:prefix(Towel, Pattern) of
+            false -> 0;
+            true -> can_fold(Towels, lists:nthtail(length(Towel), Pattern))
+        end
+    end,
+    lists:sum(lists:map(Help, Towels)).
+-endif.
 
-         lists:flatmap(Help, Towels)
-                                      end.
-
-% can_fold(Towels, []) -> [true];
-% can_fold(Towels, Pattern) ->
-%     Help = fun (Towel) -> case lists:prefix(Towel, Pattern) of
-%      false -> [false];
-%      true -> can_fold(Towels, lists:nthtail(length(Towel), Pattern))
-%                                 end
-%                 end,
-% 
-%          lists:flatmap(Help, Towels).
-
+solve(_, [], Accum) ->
+    Accum;
+solve(Towels, [Head | Tail], Accum) ->
+    Res = can_fold(Towels, Head),
+    solve(Towels, Tail, Accum + Res).
 
 main() ->
     Lines = string:lexemes(lists:flatten(help:stdin()), [$\n]),
     {Towels, Valids} = parse(Lines),
-    Filtered = lists:flatmap(fun (Valid) -> memoized_can_fold(Towels, Valid) end, Valids),
-    io:write(help:count(true, Filtered)), io:nl(),
+    Res = solve(Towels, Valids, 0),
+    io:write(Res),
+    io:nl(),
     halt().
